6ecb6d84171add85627c7b66b6ea2cffdbf88b90
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-06-06 00:02:23 -0300

    feat: :sparkles: implements global styles and defaultTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from "styled-components";
+
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
-    return <h1>Ignite timer</h1>;
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <GlobalStyle />
+        </ThemeProvider>
+    );
 }
