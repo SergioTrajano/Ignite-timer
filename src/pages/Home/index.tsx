236852e4a933fb55c9f2005762a365b1233a4b40
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-06-09 14:24:13 -0300

    feat: :sparkles: add context  to comunicate with NewTask and CountDown components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,22 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { differenceInSeconds } from "date-fns";
 import { HandPalm, Play } from "phosphor-react";
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { createContext, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import * as zod from "zod";
 
+import { CountDown } from "./components/CountDown";
+import { NewTask } from "./components/NewTask";
+
 import * as S from "./styles";
+
+interface Task {
+    id: string;
+    task: string;
+    minutesAmount: number;
+    startDate: Date;
+    interruptedDate?: Date;
+    finishedDate?: Date;
+}
 
 const newTaskSchema = zod.object({
     task: zod.string().min(1, "Informe a tarefa"),
@@ -17,21 +28,23 @@ const newTaskSchema = zod.object({
 
 type newTaskProps = zod.infer<typeof newTaskSchema>;
 
-interface Task {
-    id: string;
-    task: string;
-    minutesAmount: number;
-    startDate: Date;
-    interruptedDate?: Date;
-    finishedDate?: Date;
+interface TaskContextProps {
+    activeTask: Task | undefined;
+    activeTaskId: string | null;
+    amountSecondsPast: number;
+
+    markCurrentTaskAsFinished: () => void;
+    updateSecondsPast: (newValue: number) => void;
 }
+
+export const TaskContext = createContext<TaskContextProps>({} as TaskContextProps);
 
 export function Home() {
     const [tasks, setTasks] = useState<Task[]>([]);
     const [activeTaskId, setActiveTaskId] = useState<string | null>(null);
     const [amountSecondsPast, setAmountSecondsPast] = useState<number>(0);
 
-    const { handleSubmit, register, watch, reset } = useForm<newTaskProps>({
+    const newCycleForm = useForm<newTaskProps>({
         resolver: zodResolver(newTaskSchema),
         defaultValues: {
             task: "",
@@ -39,55 +52,29 @@ export function Home() {
         },
     });
 
+    const { handleSubmit, watch, reset } = newCycleForm;
+
     const activeTask = tasks.find((task) => task.id === activeTaskId);
-
-    const totalSeconds = activeTask ? activeTask.minutesAmount * 60 : 0;
-    const currentSeconds = activeTask ? totalSeconds - amountSecondsPast : 0;
-
-    const minutesAmount = Math.floor(currentSeconds / 60);
-    const secondsAmount = currentSeconds % 60;
-
-    const minutesLeft = String(minutesAmount).padStart(2, "0");
-    const secondsLeft = String(secondsAmount).padStart(2, "0");
-
-    useEffect(() => {
-        if (document.hidden && activeTask && currentSeconds !== 0) {
-            document.title = `${minutesLeft}: ${secondsLeft}`;
-        } else {
-            document.title = `Ignite timer`;
-        }
-    }, [activeTask, currentSeconds, minutesLeft, secondsLeft, totalSeconds]);
-
-    useEffect(() => {
-        let interval: number;
-
-        if (activeTask) {
-            interval = setInterval(() => {
-                const secondsDifference = differenceInSeconds(new Date(), activeTask.startDate);
-
-                if (secondsDifference >= totalSeconds) {
-                    setTasks((prev) =>
-                        prev.map((task) => {
-                            if (task.id === activeTaskId) {
-                                return { ...task, finishedDate: new Date() };
-                            }
-
-                            return task;
-                        })
-                    );
-
-                    setAmountSecondsPast(totalSeconds);
-                    clearInterval(interval);
-                } else {
-                    setAmountSecondsPast(secondsDifference);
-                }
-            }, 1000);
-        }
-    }, [activeTask, activeTaskId, totalSeconds]);
 
     const taskInputValue = watch("task");
     const minutesAmountInputValue = watch("minutesAmount");
     const isSubmitDisabled = !taskInputValue || !minutesAmountInputValue;
+
+    function markCurrentTaskAsFinished() {
+        setTasks((prev) =>
+            prev.map((task) => {
+                if (task.id === activeTaskId) {
+                    return { ...task, finishedDate: new Date() };
+                }
+
+                return task;
+            })
+        );
+    }
+
+    function updateSecondsPast(newValue: number) {
+        setAmountSecondsPast(newValue);
+    }
 
     function renderButtonBasedIfActiveTask() {
         if (activeTask) {
@@ -145,44 +132,21 @@ export function Home() {
     return (
         <S.HomeContainer>
             <form onSubmit={handleSubmit(handleCreateTask)}>
-                <S.FormContainer>
-                    <label htmlFor="task">Vou trabalhar em</label>
-                    <S.TaskInput
-                        id="task"
-                        type="text"
-                        placeholder="Dê um nome para o seu projeto"
-                        list="taskSuggestions"
-                        disabled={!!activeTask}
-                        {...register("task")}
-                    />
+                <TaskContext.Provider
+                    value={{
+                        activeTask,
+                        activeTaskId,
+                        amountSecondsPast,
+                        updateSecondsPast,
+                        markCurrentTaskAsFinished,
+                    }}
+                >
+                    <FormProvider {...newCycleForm}>
+                        <NewTask />
+                    </FormProvider>
 
-                    <datalist id="taskSuggestions">
-                        <option value="projeto 1" />
-                        <option value="projeto 2" />
-                        <option value="projeto 3" />
-                    </datalist>
-
-                    <label htmlFor="minutesAmount">durante</label>
-                    <S.MinutesAmountInput
-                        id="minutesAmount"
-                        type="number"
-                        placeholder="00"
-                        min={1}
-                        max={60}
-                        disabled={!!activeTask}
-                        {...register("minutesAmount", { valueAsNumber: true })}
-                    />
-
-                    <span>minutos.</span>
-                </S.FormContainer>
-
-                <S.CountDownContainer>
-                    <span>{minutesLeft[0]}</span>
-                    <span>{minutesLeft[1]}</span>
-                    <S.CountDownSeparator>:</S.CountDownSeparator>
-                    <span>{secondsLeft[0]}</span>
-                    <span>{secondsLeft[1]}</span>
-                </S.CountDownContainer>
+                    <CountDown />
+                </TaskContext.Provider>
 
                 {renderButtonBasedIfActiveTask()}
             </form>
