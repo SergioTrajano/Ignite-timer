f7b58bed04609d7503a25d53891e6322c7f8544f
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-06-08 23:48:22 -0300

    feat: :sparkles: add functionalite to count down

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Play } from "phosphor-react";
-import { useState } from "react";
+import { differenceInSeconds } from "date-fns";
+import { HandPalm, Play } from "phosphor-react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import * as zod from "zod";
 
@@ -10,7 +11,7 @@ const newTaskSchema = zod.object({
     task: zod.string().min(1, "Informe a tarefa"),
     minutesAmount: zod
         .number()
-        .min(5, "A tarefa precisa durar no mínimo 5 minutos.")
+        .min(1, "A tarefa precisa durar no mínimo 5 minutos.")
         .max(60, "A tarefa precisa durar no máximo 60 minutos."),
 });
 
@@ -20,6 +21,9 @@ interface Task {
     id: string;
     task: string;
     minutesAmount: number;
+    startDate: Date;
+    interruptedDate?: Date;
+    finishedDate?: Date;
 }
 
 export function Home() {
@@ -35,19 +39,6 @@ export function Home() {
         },
     });
 
-    function handleCreateTask(data: newTaskProps) {
-        const newTask: Task = {
-            id: String(new Date().getTime()),
-            task: data.task,
-            minutesAmount: data.minutesAmount,
-        };
-
-        setTasks((prev) => [...prev, newTask]);
-        setActiveTaskId(newTask.id);
-
-        reset();
-    }
-
     const activeTask = tasks.find((task) => task.id === activeTaskId);
 
     const totalSeconds = activeTask ? activeTask.minutesAmount * 60 : 0;
@@ -59,8 +50,97 @@ export function Home() {
     const minutesLeft = String(minutesAmount).padStart(2, "0");
     const secondsLeft = String(secondsAmount).padStart(2, "0");
 
+    useEffect(() => {
+        if (document.hidden && activeTask && currentSeconds !== 0) {
+            document.title = `${minutesLeft}: ${secondsLeft}`;
+        } else {
+            document.title = `Ignite timer`;
+        }
+    }, [activeTask, currentSeconds, minutesLeft, secondsLeft, totalSeconds]);
+
+    useEffect(() => {
+        let interval: number;
+
+        if (activeTask) {
+            interval = setInterval(() => {
+                const secondsDifference = differenceInSeconds(new Date(), activeTask.startDate);
+
+                if (secondsDifference >= totalSeconds) {
+                    setTasks((prev) =>
+                        prev.map((task) => {
+                            if (task.id === activeTaskId) {
+                                return { ...task, finishedDate: new Date() };
+                            }
+
+                            return task;
+                        })
+                    );
+
+                    setAmountSecondsPast(totalSeconds);
+                    clearInterval(interval);
+                } else {
+                    setAmountSecondsPast(secondsDifference);
+                }
+            }, 1000);
+        }
+    }, [activeTask, activeTaskId, totalSeconds]);
+
     const taskInputValue = watch("task");
-    const isSubmitDisabled = !taskInputValue;
+    const minutesAmountInputValue = watch("minutesAmount");
+    const isSubmitDisabled = !taskInputValue || !minutesAmountInputValue;
+
+    function renderButtonBasedIfActiveTask() {
+        if (activeTask) {
+            return (
+                <S.StopCountDownButton
+                    type="button"
+                    onClick={handleInterruptTask}
+                >
+                    <HandPalm size={24} />
+                    Interromper
+                </S.StopCountDownButton>
+            );
+        }
+
+        return (
+            <S.StartCountDownButton
+                type="submit"
+                disabled={isSubmitDisabled}
+            >
+                <Play size={24} />
+                Começar
+            </S.StartCountDownButton>
+        );
+    }
+
+    function handleInterruptTask() {
+        setTasks((prev) =>
+            prev.map((task) => {
+                if (activeTask?.id === activeTaskId) {
+                    return { ...task, interruptedDate: new Date() };
+                }
+
+                return task;
+            })
+        );
+
+        setActiveTaskId(null);
+    }
+
+    function handleCreateTask(data: newTaskProps) {
+        const newTask: Task = {
+            id: String(new Date().getTime()),
+            task: data.task,
+            minutesAmount: data.minutesAmount,
+            startDate: new Date(),
+        };
+
+        setTasks((prev) => [...prev, newTask]);
+        setActiveTaskId(newTask.id);
+        setAmountSecondsPast(0);
+
+        reset();
+    }
 
     return (
         <S.HomeContainer>
@@ -72,6 +152,7 @@ export function Home() {
                         type="text"
                         placeholder="Dê um nome para o seu projeto"
                         list="taskSuggestions"
+                        disabled={!!activeTask}
                         {...register("task")}
                     />
 
@@ -86,9 +167,9 @@ export function Home() {
                         id="minutesAmount"
                         type="number"
                         placeholder="00"
-                        step={5}
-                        min={0}
+                        min={1}
                         max={60}
+                        disabled={!!activeTask}
                         {...register("minutesAmount", { valueAsNumber: true })}
                     />
 
@@ -103,10 +184,7 @@ export function Home() {
                     <span>{secondsLeft[1]}</span>
                 </S.CountDownContainer>
 
-                <S.StartCountDownButton disabled={isSubmitDisabled}>
-                    <Play size={24} />
-                    Começar
-                </S.StartCountDownButton>
+                {renderButtonBasedIfActiveTask()}
             </form>
         </S.HomeContainer>
     );
